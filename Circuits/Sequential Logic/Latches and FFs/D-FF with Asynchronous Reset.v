d9f82757bef7d70320c553ce36865d1b6ebbb60d
Dev Chadha <chadhadev2002@gmail.com>
2022-09-03 17:16:04 +0530

    Asynchronous Reset

---
+++ Circuits/Sequential Logic/Latches and FFs/D-FF with Asynchronous Reset.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output reg [7:0] q
+);
+    
+      always@(posedge clk or posedge areset)
+        begin
+            if(areset) q <= 0;
+            else q <= d;
+        end
+endmodule
