8dccdaaa66b6d4a68fb6eb57e333fdb30323e1c5
Dev Chadha <chadhadev2002@gmail.com>
2023-01-16 00:12:49 +0530

    DFF Async

---
+++ Circuits/Sequential Logic/Latches and FFs/DFF asyn.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input d, 
+    input ar,   // asynchronous reset
+    output reg q);
+    
+    always @ (posedge clk, posedge ar)
+        begin
+            if (ar)
+			q <= 0;
+		else
+			q <= d;
+        end
+endmodule
